641c6054ccd86becfced79e310687d747f8e6fd4
Rashid Shethwala <abdulrashid.shethwala@thegatewaycorp.com>
2025-08-22 12:29:25 +0530

    fixed changes and ready for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ document.documentElement.setAttribute('data-theme', theme)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react_vite_portfolio/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/react_vite_portfolio/",
   plugins: [react()],
   server: { port: 5173, open: true }
 })
